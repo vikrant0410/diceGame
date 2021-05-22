55cef2a329a7eac02adcfff206996323dd1fa182
vikrant0410 <vikrantpanchal11@gmail.com>
2021-05-22 21:21:29 +0530

    initial version

---
+++ script.js
@@ -0,0 +1,48 @@
+// var randomeNumber1 = Math.round((Math.random() * 5)+1);
+
+// document.getElementById("dice-img-1").setAttribute("src","./images/dice"+ randomeNumber1 + ".svg");
+
+
+
+// var randomeNumber2 = Math.round((Math.random() * 5)+1);
+
+// document.getElementById("dice-img-2").setAttribute("src","./images/dice"+ randomeNumber2 + ".svg");
+
+
+
+
+
+
+
+// if( randomeNumber1 > randomeNumber2){
+//     document.getElementById("page-title").innerText = "player 1 Wins";
+// }
+// else if(randomeNumber1 < randomeNumber2){
+//     document.getElementById("page-title").innerText = "player 2 Wins";
+// }
+// else{
+//     document.getElementById("page-title").innerText = "Draw";
+// }
+
+
+
+// console.log(document.querySelectorAll("h1"));
+
+
+// jquery
+
+
+
+// console.log($(".page-title").hide());
+
+
+
+
+document.querySelector("#page-title").addEventListener("click",()=>{
+    document.querySelector("#page-title").innerHTML = "Hello world"
+})
+
+
+// $("#page-title").click(()=>{
+//     $("#page-title").hide()
+})
